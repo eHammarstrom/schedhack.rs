42ae34e30a223bf676dc1a98e887d50c948359f3
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2021-01-21 20:47:48 +0100

    Let's use Rust's time abstraction
    Signed-off-by: Emil Hammarstrom <emil.a.hammarstrom@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![feature(linked_list_cursors)]
+#![feature(duration_zero)]
 
-use std::sync::{Arc, Mutex};
-use std::sync::mpsc::channel;
-use std::sync::mpsc::{Sender, Receiver};
 use std::collections::LinkedList;
+use std::sync::mpsc::channel;
+use std::sync::mpsc::{Receiver, Sender};
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
-use std::boxed::Box;
 
 type DelayedWorkResult = Result<(), &'static str>;
 
@@ -16,23 +16,22 @@ type ProtectedList<T> = Arc<Mutex<LinkedList<T>>>;
 
 struct Timeout {
     work: Work,
-    ticks: u128,
-    dbg_init_ticks: u128,
-    dbg_expected_trigger: u128,
+    delay: Duration,
+    dbg_init_ticks: Duration,
+    dbg_expected_trigger: Duration,
 }
 
 impl Timeout {
-    fn new(work: Work, delay_ms: u128) -> Timeout {
+    fn new(work: Work, delay: Duration) -> Timeout {
         let current_millis = SystemTime::now()
             .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards")
-            .as_millis();
+            .expect("Time went backwards");
 
         Timeout {
             work,
-            ticks: delay_ms,
-            dbg_init_ticks: delay_ms,
-            dbg_expected_trigger: current_millis + delay_ms,
+            delay,
+            dbg_init_ticks: delay,
+            dbg_expected_trigger: current_millis + delay,
         }
     }
 }
@@ -40,7 +39,7 @@ impl Timeout {
 impl std::fmt::Debug for Timeout {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Timeout")
-            .field("ticks", &self.ticks)
+            .field("ticks", &self.delay)
             .field("initial_ticks", &self.dbg_init_ticks)
             .field("expected_trigger", &self.dbg_expected_trigger)
             .finish()
@@ -69,51 +68,31 @@ fn work_b(msg: String) {
 }
 
 fn timeout_thread(work_sender: Sender<Work>, list_protected: ProtectedList<Timeout>) {
-    // TODO: Ideally this should be implemented with an
-    // interrupt timer instead we just sleep some and keep
-    // track of the passed milliseconds to an acceptable
-    // accuracy (for this test).
-    // This is very naive, just wanted to test the scheduler
-    // logic.
-    let mut delta_micros = 0;
-    let mut last_micro = SystemTime::now();
     loop {
-
-        thread::sleep(Duration::from_micros(250));
-
-        let micros_slept = last_micro.elapsed()
-            .expect("Failed to get elapsed time")
-            .as_micros();
-
-        delta_micros += micros_slept;
-
-        last_micro = SystemTime::now();
-
-        if delta_micros < 1000 {
-            continue;
-        }
-        delta_micros -= 1000;
-
-        let mut list = list_protected.lock()
-            .expect("Failed to lock timeout queue");
+        let mut list = list_protected.lock().expect("Failed to lock timeout queue");
         let mut t = match list.front_mut() {
             Some(item) => item,
             None => continue,
         };
 
-        if let None = t.ticks.checked_sub(1) {
+        if t.delay == Duration::ZERO {
             let expired = list.pop_front().unwrap();
-            work_sender.send(expired.work)
+            work_sender
+                .send(expired.work)
                 .expect("Failed to send delayed work");
-
             let current_millis = SystemTime::now()
                 .duration_since(UNIX_EPOCH)
                 .expect("Time went backwards")
                 .as_millis();
-            println!("Expected expiration {}", expired.dbg_expected_trigger);
+            println!(
+                "Expected expiration {}",
+                expired.dbg_expected_trigger.as_millis()
+            );
             println!("Actual expiration {}", current_millis);
         } else {
-            t.ticks -= 1;
+            let sleep_duration = t.delay;
+            t.delay = Duration::ZERO;
+            thread::sleep(sleep_duration);
         }
     }
 }
@@ -124,29 +103,32 @@ fn worker_thread(work_receiver: Receiver<Work>) {
             Ok(mut work) => {
                 println!("Executing work,");
                 work();
-            },
+            }
             Err(e) => {
                 println!("{:?}", e);
-                break
+                break;
             }
         }
     }
 }
 
-fn delayed_work_submit(work: Work, delay_ms: u128, list_protected: &mut ProtectedList<Timeout>) -> DelayedWorkResult {
+fn delayed_work_submit(
+    work: Work,
+    delay: Duration,
+    list_protected: &mut ProtectedList<Timeout>,
+) -> DelayedWorkResult {
     let mut list = list_protected.lock().or_else(|_| Err("Lock failed"))?;
-    let mut new = Timeout::new(work, delay_ms);
+    let mut new = Timeout::new(work, delay);
     let mut list_cursor = list.cursor_front_mut();
 
     while let Some(t) = list_cursor.current() {
-
-        if t.ticks > new.ticks {
-            t.ticks -= new.ticks;
+        if t.delay > new.delay {
+            t.delay -= new.delay;
             list_cursor.insert_before(new);
             return Ok(());
         }
 
-        new.ticks -= t.ticks;
+        new.delay -= t.delay;
 
         list_cursor.move_next();
     }
@@ -169,23 +151,36 @@ fn main() {
 
     thread::sleep(Duration::from_millis(100));
 
-    work_sender.send(Box::new(|| {
-        work_a(64)
-    })).unwrap();
+    work_sender.send(Box::new(|| work_a(64))).unwrap();
 
-    work_sender.send(Box::new(|| {
-        work_b("From main".to_string())
-    })).unwrap();
+    work_sender
+        .send(Box::new(|| work_b("From main".to_string())))
+        .unwrap();
 
-    delayed_work_submit(Box::new(|| {
-        work_b("Hello, 200ms later!".to_string());
-    }), 200, &mut list_lock).unwrap();
-    delayed_work_submit(Box::new(|| {
-        work_b("Hello, 50ms later!".to_string());
-    }), 50, &mut list_lock).unwrap();
-    delayed_work_submit(Box::new(|| {
-        work_b("Hello, 100ms later!".to_string());
-    }), 100, &mut list_lock).unwrap();
+    delayed_work_submit(
+        Box::new(|| {
+            work_b("Hello, 200ms later!".to_string());
+        }),
+        Duration::from_millis(200),
+        &mut list_lock,
+    )
+    .unwrap();
+    delayed_work_submit(
+        Box::new(|| {
+            work_b("Hello, 50ms later!".to_string());
+        }),
+        Duration::from_millis(50),
+        &mut list_lock,
+    )
+    .unwrap();
+    delayed_work_submit(
+        Box::new(|| {
+            work_b("Hello, 100ms later!".to_string());
+        }),
+        Duration::from_millis(100),
+        &mut list_lock,
+    )
+    .unwrap();
 
     worker_thread.join().unwrap()
 }
