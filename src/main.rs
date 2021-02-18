6c89f18fec7c5dc8967a3485b7a786b9c4150b45
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2021-02-18 17:36:27 +0100

    Move timeouts list into timekeeper thread
    * Removed superfluous timekeeper struct
    
    Signed-off-by: Emil Hammarstrom <emil.a.hammarstrom@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,22 @@
 #![feature(linked_list_cursors)]
 #![feature(linked_list_remove)]
 #![feature(duration_zero)]
+#![feature(duration_constants)]
 
 use std::collections::LinkedList;
 use std::sync::mpsc::channel;
 use std::sync::mpsc::{Receiver, Sender};
-use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
-type DelayedWorkResult = Result<(), &'static str>;
-
 type Work = Box<dyn FnMut() + Send + 'static>;
 
-type ProtectedList<T> = Arc<Mutex<LinkedList<T>>>;
-
-struct TimeoutWorker<T> {
-    timeouts: ProtectedList<T>,
-    notify_sender: Sender<()>,
-}
-
 struct Timeout {
-    id: usize,
     work: Work,
     delay: Duration,
     dbg_init_ticks: Duration,
     dbg_expected_trigger: Duration,
 }
-
-impl PartialEq for Timeout {
-    fn eq(&self, rhs: &Self) -> bool {
-        return self.id == rhs.id;
-    }
-}
-
-static mut ID_COUNTER: usize = 0;
 
 impl Timeout {
     fn new(work: Work, delay: Duration) -> Timeout {
@@ -42,9 +24,7 @@ impl Timeout {
             .duration_since(UNIX_EPOCH)
             .expect("Time went backwards");
 
-        unsafe { ID_COUNTER += 1 };
         Timeout {
-            id: unsafe { ID_COUNTER },
             work,
             delay,
             dbg_init_ticks: delay,
@@ -63,83 +43,67 @@ impl std::fmt::Debug for Timeout {
     }
 }
 
-fn timeouts_remove(id: usize, timeouts: &mut LinkedList<Timeout>) -> Option<Timeout> {
-    let mut idx = -1;
-    for (i, t) in timeouts.iter().enumerate() {
-        if id == t.id {
-            idx = i as isize;
-            break;
+fn timeouts_add_timeout(list: &mut LinkedList<Timeout>, mut new: Timeout) {
+    let mut list_cursor = list.cursor_front_mut();
+
+    while let Some(t) = list_cursor.current() {
+        if t.delay > new.delay {
+            t.delay -= new.delay;
+            list_cursor.insert_before(new);
+            return;
         }
+
+        new.delay -= t.delay;
+
+        list_cursor.move_next();
     }
-    if idx >= 0 {
-        return Some(timeouts.remove(idx as usize));
-    }
-    return None;
+
+    list_cursor.insert_after(new);
 }
 
-fn timeouts_decr_delay(id: usize, change: Duration, timeouts: &mut LinkedList<Timeout>) {
-    for t in timeouts.iter_mut() {
-        if t.id == id {
-            if change > t.delay {
-                t.delay = Duration::ZERO;
-            } else {
-                t.delay -= change;
-            }
-        }
-    }
-}
+fn timekeeper_thread(work_sender: Sender<Work>, notify_receiver: Receiver<Timeout>) {
+    let mut list: LinkedList<Timeout> = LinkedList::new();
 
-fn timekeeper_thread(
-    work_sender: Sender<Work>,
-    notify_receiver: Receiver<()>,
-    list_protected: ProtectedList<Timeout>,
-) {
     loop {
-        let timeout_info = {
-            let list = list_protected.lock().expect("Failed to lock timeout queue");
-            list.front().map(|t| (t.id, t.delay))
+        let mut timeout = match list.pop_front() {
+            Some(t) => t,
+            None => notify_receiver.recv().expect("Failed to receive timeout"),
         };
 
-        /* We have no items, wait for a notification */
-        if timeout_info.is_none() {
-            notify_receiver.recv().expect("Fail'd to recv notification");
-            continue;
-        }
-
-        let (t_id, t_delay) = timeout_info.unwrap();
         let sleep_time = SystemTime::now();
-
-        match notify_receiver.recv_timeout(t_delay) {
+        match notify_receiver.recv_timeout(timeout.delay) {
             /* We didn't get to wait, let's reduce the time of this work
              * by the amount of time waited so far.
              */
-            Ok(_) => {
-                let mut list = list_protected.lock().expect("Failed to lock timeout queue");
+            Ok(new_timeout) => {
                 let slept = sleep_time.elapsed().unwrap();
-                timeouts_decr_delay(t_id, slept, &mut list);
+                timeout.delay -= slept;
+                list.push_front(timeout);
+                timeouts_add_timeout(&mut list, new_timeout);
             }
 
             /* Timed out, let's process the work and continue */
             Err(_) => {
-                let mut list = list_protected.lock().expect("Failed to lock timeout queue");
-                if let Some(expired) = timeouts_remove(t_id, &mut list) {
-                    work_sender
-                        .send(expired.work)
-                        .expect("Failed to send delayed work");
+                let now = SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .expect("Time went backwards")
+                    .as_millis();
 
-                    let now = SystemTime::now()
-                        .duration_since(UNIX_EPOCH)
-                        .expect("Time went backwards")
-                        .as_millis();
+                work_sender
+                    .send(timeout.work)
+                    .expect("Failed to send delayed work");
 
+                println!(
+                    "Expected expiration {}",
+                    timeout.dbg_expected_trigger.as_millis()
+                );
+                println!("Actual expiration {}", now);
+                if now < timeout.dbg_expected_trigger.as_millis() {
+                    println!("TOO EARLY");
+                } else {
                     println!(
-                        "Expected expiration {}",
-                        expired.dbg_expected_trigger.as_millis()
-                    );
-                    println!("Actual expiration {}", now);
-                    println!(
-                        "Target missed by {}",
-                        now - expired.dbg_expected_trigger.as_millis()
+                        "Target missed by {:?}ms",
+                        now - timeout.dbg_expected_trigger.as_millis()
                     );
                 }
             }
@@ -159,73 +123,18 @@ fn worker_thread(work_receiver: Receiver<Work>) {
     }
 }
 
-fn delayed_work_submit(
-    work: Work,
-    delay: Duration,
-    timeout_worker: &mut TimeoutWorker<Timeout>,
-) -> DelayedWorkResult {
-    let mut list = timeout_worker
-        .timeouts
-        .lock()
-        .or_else(|_| Err("Lock failed"))?;
-    let mut new = Timeout::new(work, delay);
-    let mut list_cursor = list.cursor_front_mut();
-
-    while let Some(t) = list_cursor.current() {
-        if t.delay > new.delay {
-            t.delay -= new.delay;
-            list_cursor.insert_before(new);
-
-            /* Addition at start of queue, re-trigger scheduling */
-            if let Some(1) = list_cursor.index() {
-                println!("Queue item added to front of queue!");
-                timeout_worker
-                    .notify_sender
-                    .send(())
-                    .expect("Fail'd to send notification");
-            }
-
-            return Ok(());
-        }
-
-        new.delay -= t.delay;
-
-        list_cursor.move_next();
-    }
-
-    list_cursor.insert_after(new);
-
-    /* Addition at start of queue, re-trigger scheduling */
-    if let None = list_cursor.index() {
-        println!("First queue item was added!");
-        timeout_worker
-            .notify_sender
-            .send(())
-            .expect("Fail'd to send notification");
-    }
-
-    Ok(())
-}
-
 fn main() {
     let (work_sender, work_receiver) = channel();
-    let (notify_sender, notify_receiver) = channel();
-    let list_lock: ProtectedList<Timeout> = Arc::new(Mutex::new(LinkedList::new()));
+    let (timeout_work_sender, timeout_work_receiver) = channel();
 
     /* Startup work processor */
     let worker_thread = thread::spawn(|| worker_thread(work_receiver));
 
     /* Startup timekeeper for delayed work */
     {
-        let list_lock = list_lock.clone();
         let work_sender = work_sender.clone();
-        thread::spawn(|| timekeeper_thread(work_sender, notify_receiver, list_lock));
+        thread::spawn(|| timekeeper_thread(work_sender, timeout_work_receiver));
     }
-
-    let mut timeout_worker = TimeoutWorker {
-        timeouts: list_lock,
-        notify_sender,
-    };
 
     thread::sleep(Duration::from_millis(100));
 
@@ -243,41 +152,41 @@ fn main() {
         .send(Box::new(|| work_b("From main".to_string())))
         .unwrap();
 
-    delayed_work_submit(
-        Box::new(|| {
-            work_b("Hello, 200ms later!".to_string());
-        }),
-        Duration::from_millis(200),
-        &mut timeout_worker,
-    )
-    .unwrap();
-    delayed_work_submit(
-        Box::new(|| {
-            work_b("Hello, 50ms later!".to_string());
-        }),
-        Duration::from_millis(50),
-        &mut timeout_worker,
-    )
-    .unwrap();
-    delayed_work_submit(
-        Box::new(|| {
-            work_b("Hello, 100ms later!".to_string());
-        }),
-        Duration::from_millis(100),
-        &mut timeout_worker,
-    )
-    .unwrap();
+    timeout_work_sender
+        .send(Timeout::new(
+            Box::new(|| {
+                work_b("Hello, 200ms later!".to_string());
+            }),
+            Duration::from_millis(200),
+        ))
+        .unwrap();
+    timeout_work_sender
+        .send(Timeout::new(
+            Box::new(|| {
+                work_b("Hello, 50ms later!".to_string());
+            }),
+            Duration::from_millis(50),
+        ))
+        .unwrap();
+    timeout_work_sender
+        .send(Timeout::new(
+            Box::new(|| {
+                work_b("Hello, 100ms later!".to_string());
+            }),
+            Duration::from_millis(100),
+        ))
+        .unwrap();
 
     thread::sleep(Duration::from_millis(10));
 
-    delayed_work_submit(
-        Box::new(|| {
-            work_b("Hello, 20ms later!".to_string());
-        }),
-        Duration::from_millis(20),
-        &mut timeout_worker,
-    )
-    .unwrap();
+    timeout_work_sender
+        .send(Timeout::new(
+            Box::new(|| {
+                work_b("Hello, 20ms later!".to_string());
+            }),
+            Duration::from_millis(20),
+        ))
+        .unwrap();
 
     worker_thread.join().unwrap()
 }
